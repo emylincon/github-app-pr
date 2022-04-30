3bb9b0e7da8a0a5e73847b8fb9b142af855a68d8
Emeka E Ugwuanyi <ugwuanye@lsbu.ac.uk>
2022-04-30 16:39:03 +0100

    code enhancement (#19)

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from github import Github, GithubIntegration
 
 
 app = Flask(__name__)
+TABLE = {}  # ! schema: {pull_request_url: true} {str: bool}
 
 # Read Environment Variables
 app_id = os.getenv("app_id")
@@ -24,6 +25,13 @@ git_integration = GithubIntegration(
 )
 
 
+def error_handler(e: Exception, kind: str = "CRITICAL") -> str:
+    err = f"[ERROR][{kind}]: {e.__class__} | message: {e}"
+    print(err)
+    print(f"[ERROR DETAILS][{kind}]: {e.__traceback__}")
+    return err
+
+
 @app.route("/", methods=["GET"])
 def home():
     return "Hello 👋"
@@ -31,45 +39,64 @@ def home():
 
 @app.route("/", methods=["POST"])
 def bot():
-    # Get the event payload
+    # * Get the event payload
     payload = request.json
 
-    # Check if the event is a GitHub PR creation event
+    # * Check if the event is a GitHub PR creation event
     if (
         not all(k in payload.keys() for k in ["action", "pull_request"])
         and payload["action"] == "opened"
     ):
         return "ok"
 
-    owner = payload["repository"]["owner"]["login"]
-    repo_name = payload["repository"]["name"]
-
-    # Get a git connection as our bot
-    # Here is where we are getting the permission to talk as our bot and not
-    # as a Python webservice
-    git_connection = Github(
-        login_or_token=git_integration.get_access_token(
-            git_integration.get_installation(owner, repo_name).id
-        ).token
-    )
-    repo = git_connection.get_repo(f"{owner}/{repo_name}")
-
-    issue = repo.get_issue(number=payload["pull_request"]["number"])
-
-    # Create a comment on the PR
-    issue.create_comment(f":rainbow: Thanks for creating this PR :rocket:")
-
-    # Add a Label to the PR
-    issue.set_labels("enhancement")
-
-    # Approve PR
     try:
-        pr = repo.get_pull(number=payload["pull_request"]["number"])
-        commit = repo.get_commit(pr.head.sha)
-        pr.create_review(commit=commit, body="Approved  👍", event="APPROVE")
+        owner = payload["repository"]["owner"]["login"]
+        repo_name = payload["repository"]["name"]
+        pr_url = payload["pull_request"]["html_url"]
+        pr_state = payload["pull_request"]["state"]
     except Exception as e:
-        print("[ERROR]:", e.__class__)
-        print("Please ensure git repository is not private")
+        return error_handler(e, "PAYLOAD")
+
+    # if request has not been processed and PR state is open
+    if not TABLE.get(pr_url) and pr_state == "open":
+        # Get a git connection as our bot
+        # Here is where we are getting the permission to talk as our bot and not
+        # as a Python webservice
+        git_connection = Github(
+            login_or_token=git_integration.get_access_token(
+                git_integration.get_installation(owner, repo_name).id
+            ).token
+        )
+        repo = git_connection.get_repo(f"{owner}/{repo_name}")
+
+        issue = repo.get_issue(number=payload["pull_request"]["number"])
+
+        # Add a Label to the PR
+        try:
+            issue.set_labels("enhancement")
+        except Exception as e:
+            error_handler(e, "ADD LABEL")
+
+        # Create a comment on the PR
+        try:
+            issue.create_comment(
+                f":rainbow: Thanks for creating this PR :rocket:")
+        except Exception as e:
+            error_handler(e, "CREATE COMMENT")
+
+        # set Pull request to processed
+        TABLE[pr_url] = True
+
+        # Approve PR
+        try:
+            pr = repo.get_pull(number=payload["pull_request"]["number"])
+            commit = repo.get_commit(pr.head.sha)
+            pr.create_review(
+                commit=commit, body="Approved  👍", event="APPROVE")
+        except Exception as e:
+            return error_handler(e, "APPROVE PULL REQUEST")
+    else:
+        print("[INFO]: REQUEST HAS BEEN PREVIOUSLY SUBMITTED")
 
     return "ok"
 
